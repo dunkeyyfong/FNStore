a0657c0dae01fd966a1ce044d7c42b509d2438ef
Fong <126961584+dunkeyyfong@users.noreply.github.com>
2023-11-28 22:40:54 +0700

    create folder and create views

---
+++ FNStore/src/Views/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  FNStore
+//
+//  Created by DunkeyyFong on 28/11/2023.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
