51ce0bf63dfe564081e9808ec04d84de6cc2e887
Fong <126961584+dunkeyyfong@users.noreply.github.com>
2023-11-28 22:28:36 +0700

    Initial Commit

---
+++ FNStore/FNStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FNStoreApp.swift
+//  FNStore
+//
+//  Created by DunkeyyFong on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FNStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
